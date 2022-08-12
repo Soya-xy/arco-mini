c36dc7f67b4230508f893e54ca445e34a90ac191
Soya <3100034521@qq.com>
2022-08-12 10:39:32 +0800

    添加 datePicker

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,11 +3,13 @@
 </script>
 
 <template>
-  <div>
-    <Menu />
-    <div class="flex" w="full" justify="between">
-      <List title="执行记录" />
-      <List title="上报记录" />
-    </div>
-  </div>
+  <router-link to="test" class="text-xl">
+    跳转到Select
+  </router-link>
+  <br>
+  <br>
+  <br>
+  <router-link to="datetest" class="text-xl">
+    跳转到datePicker
+  </router-link>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Index from '~/pages/index.vue'
-import Test from '~/pages/test.vue'
+import Test from '~/pages/selectTest.vue'
+import dateTest from '~/pages/datetest.vue'
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     { path: '/', component: Index },
     { path: '/test', component: Test },
+    { path: '/datetest', component: dateTest },
   ],
 })
 
